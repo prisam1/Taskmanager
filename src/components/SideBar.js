aead4308787e4c88f2a8faac6796f1b12177a809
prisam1 <pritamsamaddar840@gmail.com>
2023-08-27 22:40:30 +0530

    done

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,7 @@ const Sidebar = ({ openModal, darkMode }) => {
   
   return (
     <div className={`sidebar ${darkMode ? 'dark-mode' : ''}`}>
-      <button className="sidebar-button">New Task</button>
+      <button className="sidebar-buttons">New Task</button>
       <button className="sidebar-button" onClick={openModal}>
         Create Task
       </button>
