fcdd4e19416b1ad8ea8d70f98dca9751bd66c66d
prisam1 <pritamsamaddar840@gmail.com>
2023-08-31 20:16:54 +0530

    done

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -4,8 +4,10 @@ const TaskItem = ({ task, toggleComplete, editTask, deleteTask }) => {
 
   return (
     <div className={`task-item ${task.completed ? 'completed' : ''}`}>
+      <div className='tasks'>
       <h3>{task.title}</h3>
       <p>{task.description}</p>
+      </div>
       <button onClick={() => toggleComplete(task.id,)}>
        {!task.completed ? 'Mark as Completed' : 'Mark as Incomplete'}</button>
       <button onClick={() => editTask(task)}>Edit</button>
